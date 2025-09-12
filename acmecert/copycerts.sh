be2e44e0998d39378ce86acc8e4a6205c7cab932
Erik Olsen <erik@o-sit.com>
2025-09-12 15:03:04 +0200

    add copycerts

---
+++ acmecert/copycerts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define source and destination pairs
+declare -A paths=(
+  ["/home/eolsen/acmecerts/k8sfnatt/cert.p12"]="/home/erik/acmecerts/k8sfnatt/"
+  ["/home/eolsen/acmecerts/other/cert.p12"]="/home/erik/acmecerts/other/"
+  ["/home/eolsen/acmecerts/polarismedia/cert.p12"]="/home/erik/acmecerts/polarismedia/"
+  ["/home/eolsen/acmecerts/prsubdomain1/cert.p12"]="/home/erik/acmecerts/prsubdomain1/"
+  ["/home/eolsen/acmecerts/prsubdomain2/cert.p12"]="/home/erik/acmecerts/prsubdomain2/"
+)
+
+remote="eolsen@it-devops-1.adresseavisen.no"
+
+for src in "${!paths[@]}"; do
+  dest="${paths[$src]}"
+  file="$dest/cert.p12"
+
+  # If cert.p12 already exists, rename it with a timestamp
+  if [ -f "$file" ]; then
+    mv "$file" "$dest/cert_$(date +%Y%m%d_%H%M%S).p12"
+    echo "Renamed existing cert in $dest"
+  fi
+
+  # Copy the new file
+  scp "$remote:$src" "$dest"
+done
